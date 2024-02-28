328d2ef24166482c09746ad8cd39167c5f19aed8
Kavindu Sachinthe <126804361+kavindus0@users.noreply.github.com>
2024-02-28 22:01:58 +0530

    refactor code file

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:yummycakes/screens/init.dart';
+import 'package:yummycakes/cridentialScreen/init.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 void main() {
