611bcd09e5eb1b38dc8522f30f97b19135709ce0
Kavindu Sachinthe <126804361+kavindus0@users.noreply.github.com>
2024-02-29 14:03:21 +0530

    create Screen for Search page

---
+++ lib/homePage/Search.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class SearchCakes extends StatefulWidget {
+  const SearchCakes({super.key});
+
+  @override
+  State<SearchCakes> createState() => _SearchCakesState();
+}
+
+class _SearchCakesState extends State<SearchCakes> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
