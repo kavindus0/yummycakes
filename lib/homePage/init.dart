a9ecbf1a6b32e9edcd310953913fce4b9b99e73e
Kavindu Sachinthe <126804361+kavindus0@users.noreply.github.com>
2024-02-29 09:56:24 +0530

    - change State of home page - added BottomNavigationBar and It's Items, - Customize BottomNavigationBar - Change Theme Of HomePage

--- lib/homePage/init.dart
+++ lib/homePage/init.dart
@@ -1,13 +1,48 @@
 import 'package:flutter/material.dart';
 
-class HomePageScreen extends StatelessWidget {
+class HomePageScreen extends StatefulWidget {
   const HomePageScreen({super.key});
 
+  @override
+  State<HomePageScreen> createState() => _HomePageScreenState();
+}
+
+class _HomePageScreenState extends State<HomePageScreen> {
+  int index = 0;
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: true,
       home: Scaffold(
+        bottomNavigationBar: BottomNavigationBar(
+          currentIndex: 3,
+          selectedLabelStyle: TextStyle(color: Colors.black),
+          selectedItemColor: Colors.black,
+          unselectedItemColor: const Color(0xFF8B7658),
+          unselectedLabelStyle: const TextStyle(
+            color: Color(0xFF8B7658),
+          ),
+          items: const <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+              icon: Icon(
+                Icons.home,
+              ),
+              label: 'Home',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.search),
+              label: 'Search',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.person),
+              label: 'Profile',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.featured_play_list),
+              label: 'Orders',
+            ),
+          ],
+        ),
         backgroundColor: const Color.fromARGB(255, 252, 250, 248),
         appBar: AppBar(
           backgroundColor: const Color.fromARGB(255, 252, 250, 248),
