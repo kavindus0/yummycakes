e195cdd176db15711a0226b1dd9846e3a3b0263b
Kavindu Sachinthe <126804361+kavindus0@users.noreply.github.com>
2024-02-29 09:35:28 +0530

    - refactor code - Add Search Bar - Customize App Home Screen

--- lib/homePage/init.dart
+++ lib/homePage/init.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/cupertino.dart';
 
 class HomePageScreen extends StatelessWidget {
   const HomePageScreen({super.key});
@@ -9,21 +8,37 @@ class HomePageScreen extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: true,
       home: Scaffold(
+        backgroundColor: const Color.fromARGB(255, 252, 250, 248),
         appBar: AppBar(
+          backgroundColor: const Color.fromARGB(255, 252, 250, 248),
           title: const Row(
             children: [
+              Spacer(),
               IconButton(
-                icon: Icon(Icons.arrow_back_ios),
+                icon: Icon(Icons.shopping_cart_outlined, color: Colors.black),
                 onPressed: null,
               ),
-              Spacer(),
-              Text('Home Page'),
-              Spacer()
             ],
           ),
         ),
-        body: const Center(
-          child: Text('Homes Page'),
+        body: const Column(
+          children: [
+            Padding(
+                padding: EdgeInsets.symmetric(horizontal: 16.0),
+                child: SearchBar(
+                  hintText: ("Search..."),
+                  leading: Padding(
+                    padding: EdgeInsets.all(8.0),
+                    child:
+                        Icon(Icons.search_outlined, color: Color(0xFF5F5F5F)),
+                  ),
+                  elevation: MaterialStatePropertyAll(0.0),
+                  backgroundColor: MaterialStatePropertyAll(
+                    Color(0xFFF1ECE8),
+                  ),
+                )),
+            Text('Homes Page'),
+          ],
         ),
       ),
     );
