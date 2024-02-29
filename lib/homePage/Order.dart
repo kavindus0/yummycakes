0d0b7e2e874fdd72fe9af38cc5783d4b8b6f9c3a
Kavindu Sachinthe <126804361+kavindus0@users.noreply.github.com>
2024-02-29 14:03:30 +0530

    create Screen for Order page

---
+++ lib/homePage/Order.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CakeOrders extends StatefulWidget {
+  const CakeOrders({super.key});
+
+  @override
+  State<CakeOrders> createState() => _CakeOrdersState();
+}
+
+class _CakeOrdersState extends State<CakeOrders> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
