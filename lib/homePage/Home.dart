af7863c18d4dbac46920527c0f3e5ddef53136db
Kavindu Sachinthe <126804361+kavindus0@users.noreply.github.com>
2024-03-02 22:26:52 +0530

    ReDesign HomePage

--- lib/homePage/Home.dart
+++ lib/homePage/Home.dart
@@ -7,19 +7,6 @@ class HomeMain extends StatelessWidget {
   Widget build(BuildContext context) {
     return const Column(
       children: [
-        Padding(
-            padding: EdgeInsets.symmetric(horizontal: 16.0),
-            child: SearchBar(
-              hintText: ("Search..."),
-              leading: Padding(
-                padding: EdgeInsets.all(8.0),
-                child: Icon(Icons.search_outlined, color: Color(0xFF5F5F5F)),
-              ),
-              elevation: MaterialStatePropertyAll(0.0),
-              backgroundColor: MaterialStatePropertyAll(
-                Color(0xFFF1ECE8),
-              ),
-            )),
         Text('Homes Page'),
       ],
     );
