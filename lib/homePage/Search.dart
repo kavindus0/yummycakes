896e1df10c468780bc493d47691ff3ef18b1543a
Kavindu Sachinthe <126804361+kavindus0@users.noreply.github.com>
2024-03-02 22:27:46 +0530

    Search Page Section Basic Layout Created

--- lib/homePage/Search.dart
+++ lib/homePage/Search.dart
@@ -10,8 +10,23 @@ class SearchCakes extends StatefulWidget {
 class _SearchCakesState extends State<SearchCakes> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder(
-      color: Colors.cyan,
+    return const Column(
+      children: [
+        Padding(
+            padding: EdgeInsets.symmetric(horizontal: 16.0),
+            child: SearchBar(
+              hintText: ("Search..."),
+              leading: Padding(
+                padding: EdgeInsets.all(8.0),
+                child: Icon(Icons.search_outlined, color: Color(0xFF5F5F5F)),
+              ),
+              elevation: MaterialStatePropertyAll(0.0),
+              backgroundColor: MaterialStatePropertyAll(
+                Color(0xFFF1ECE8),
+              ),
+            )),
+        Text('Homes Page'),
+      ],
     );
   }
 }
