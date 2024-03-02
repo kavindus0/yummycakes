88e711dd41af24366f2af1f00e01d834fd9184da
Kavindu Sachinthe <126804361+kavindus0@users.noreply.github.com>
2024-03-02 22:33:22 +0530

    Make color list as constant

---
+++ lib/cridentialScreen/kValues.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+const List<Color> kInitPageColorGradient = [
+  Color.fromARGB(255, 0, 0, 0),
+  Color.fromARGB(255, 15, 15, 15),
+  Color.fromARGB(255, 99, 99, 99),
+  Color.fromARGB(255, 125, 125, 125),
+  Color.fromARGB(255, 199, 199, 199),
+  Color.fromARGB(255, 210, 210, 210),
+  Color.fromARGB(255, 255, 255, 255),
+];
