8ce2111ef9e3430fd752e61185d94b7bb28d548b
Kavindu Sachinthe <126804361+kavindus0@users.noreply.github.com>
2024-03-02 21:09:10 +0530

    design profile section

--- lib/homePage/Profile.dart
+++ lib/homePage/Profile.dart
@@ -10,8 +10,39 @@ class Profile extends StatefulWidget {
 class _ProfileState extends State<Profile> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder(
-      color: Colors.green,
+    return Scaffold(
+      appBar: null,
+      body: Container(
+        child: Column(
+          children: <Widget>[
+            Row(
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Container(
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      border: Border.all(
+                        color: Colors.black,
+                        width: 2,
+                      ),
+                    ),
+                    child: Text("Profile"),
+                    // Image.asset(
+                    //   'asset/img/profile/pp.png',
+                    //   width: 100,
+                    //   height: 100,
+                    //   fit: BoxFit.cover,
+                    // ),
+                  ),
+                ),
+                Text("Profile"),
+              ],
+            ),
+            Text("Profile"),
+          ],
+        ),
+      ),
     );
   }
 }
