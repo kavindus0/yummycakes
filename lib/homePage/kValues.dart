23c0f9d1e79db4db9f994160ccca0cdd134b1fc9
Kavindu Sachinthe <126804361+kavindus0@users.noreply.github.com>
2024-03-02 22:51:43 +0530

    New dart file created for storing constant values

---
+++ lib/homePage/kValues.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+const String kSearchBarText = "Search...";
+const Color kSearchBarIconColor = Color(0xFF5F5F5F);
+const Color kPageBackgroundColor = Color(0xFFF1ECE8);
+const String kHomePageTextWidgetData = 'Homes Page';
